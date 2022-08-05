685a38db8e09eed425d910852cf0858a1b8a73b8
Adrian Garcia <agarciacarillo@gmail.com>
2022-08-05 16:29:15 -0500

    improved paddle class

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ screen.title('PONG')
 screen.setup(width=800, height=600)
 screen.tracer(0)
 
-paddle = Paddle()
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 screen.listen()
-screen.onkeypress(paddle.go_up, 'Up')
-screen.onkeypress(paddle.go_down, 'Down')
+screen.onkeypress(r_paddle.go_up, 'Up')
+screen.onkeypress(r_paddle.go_down, 'Down')
+screen.onkeypress(l_paddle.go_up, 'w')
+screen.onkeypress(l_paddle.go_down, 's')
 
 game_is_on = True
 while game_is_on:
--- paddle.py
+++ paddle.py
@@ -2,16 +2,15 @@ from turtle import Turtle, width
 
 WIDTH = 5
 HEIGTH = 1
-X_POSITION = 350
 
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, position: tuple):
         super().__init__()
         self.penup()
         self.color('white')
         self.shape('square')
         self.shapesize(stretch_wid=WIDTH, stretch_len=HEIGTH)
-        self.goto(x=X_POSITION, y=0)
+        self.goto(position)
 
     def go_up(self):
         new_y = self.ycor() + 10
