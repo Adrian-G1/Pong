1bcc3955e33170a7b303b10f8286054dc1ee12d9
Adrian Garcia <agarciacarillo@gmail.com>
2022-08-07 14:32:03 -0500

    implemented wall bounce

--- ball.py
+++ ball.py
@@ -1,14 +1,18 @@
 from turtle import Turtle
 
+STARTING_ANGLE = 45
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
         self.penup()
         self.color('white')
         self.shape('circle')
+        self.angle = STARTING_ANGLE
         
 
     def move(self):
-        new_x = self.xcor() + 10
-        new_y = self.ycor() + 10
-        self.goto(new_x, new_y)
+        self.setheading(self.angle)
+        self.forward(10)
+
+    def bounce(self):
+        self.angle = 360 - self.angle
